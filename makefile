5f1d00eb916a0633372670143f97485235bbfb09
Chris Drew <chris.drew.cd@gmai.com>
2020-10-18 22:31:05 -0600

    Added stuff

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-CC=gcc
-CFLAGS=-I.
+CC=g++
+CFLAGS=-I. -std=c++11
 LDFLAGS=-l glut -l GL -l GLU -l stdc++
 
 # Triangle
@@ -21,6 +21,9 @@ triangle: $(TRIANGLE_INPUT)
 sphere: $(INPUT)
 	 $(CC) -o $(SPHERE_BINARY) $(SPHERE_INPUT) $(LDFLAGS) $(CFLAGS)
 
+spherec: $(INPUT)
+	 $(CC) -o $(SPHEREC_BINARY) $(SPHEREC_INPUT) $(LDFLAGS) $(CFLAGS)
+
 .PHONY: clean_triangle
 clean_triangle:
 	rm -rf $(TRIANGLE_BINARY)
@@ -28,6 +31,10 @@ clean_triangle:
 .PHONY: clean_sphere
 clean_sphere:
 	rm -rf $(SPHERE_BINARY)
+
+.PHONY: clean_spherec
+clean_spherec:
+	rm -rf $(SPHEREC_BINARY)
 
 .PHONY: clean
 clean:
--- sphere.cpp
+++ sphere.cpp
@@ -6,9 +6,22 @@
 
 #include <cmath>
 #include <cstdio>
+#include <vector>
+#include <time.h>
+#include <chrono>
+
+#define PI 3.14159265
+
+using namespace std;
+using namespace std::chrono;
 
 GLfloat xRotated, yRotated, zRotated;
 GLdouble radius=1;
+bool is_first_time = true;
+int prev_mouse_x, prev_mouse_y, dx, dy;
+vector<int> xarr;
+vector<int> yarr;
+high_resolution_clock::time_point time_old;
 
 void redisplayFunc(void) {
 
@@ -17,27 +30,20 @@ void redisplayFunc(void) {
     glLoadIdentity();
     glTranslatef(0.0,0.0,-8.0);
     glColor3f(0.2, 0.8, 0.1);
-    glRotatef(xRotated,0.0,0.0,1.0);
-    glRotatef(yRotated,0.0,0.0,1.0);
+    gluLookAt(1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0);
+    glRotatef(xRotated,1.0,0.0,0.0);
+    glRotatef(yRotated,0.0,1.0,0.0);
     glRotatef(zRotated,0.0,0.0,1.0);
     glScalef(1.0,1.0,1.0);
-    gluLookAt(1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0);
     glutSolidSphere(radius,20,20);
     glFlush();
 
+    // x
     glColor3f(1.0,0.0,0.0); // red x
     glBegin(GL_LINES);
-    // x aix
 
     glVertex3f(-4.0, 0.0f, 0.0f);
     glVertex3f(4.0, 0.0f, 0.0f);
-
-    // arrow
-    glVertex3f(4.0, 0.0f, 0.0f);
-    glVertex3f(3.0, 1.0f, 0.0f);
-
-    glVertex3f(4.0, 0.0f, 0.0f);
-    glVertex3f(3.0, -1.0f, 0.0f);
     glEnd();
     glFlush();
 
@@ -46,13 +52,6 @@ void redisplayFunc(void) {
     glBegin(GL_LINES);
     glVertex3f(0.0, -4.0f, 0.0f);
     glVertex3f(0.0, 4.0f, 0.0f);
-
-    // arrow
-    glVertex3f(0.0, 4.0f, 0.0f);
-    glVertex3f(1.0, 3.0f, 0.0f);
-
-    glVertex3f(0.0, 4.0f, 0.0f);
-    glVertex3f(-1.0, 3.0f, 0.0f);
     glEnd();
     glFlush();
 
@@ -61,13 +60,6 @@ void redisplayFunc(void) {
     glBegin(GL_LINES);
     glVertex3f(0.0, 0.0f ,-4.0f );
     glVertex3f(0.0, 0.0f ,4.0f );
-
-    // arrow
-    glVertex3f(0.0, 0.0f ,4.0f );
-    glVertex3f(0.0, 1.0f ,3.0f );
-
-    glVertex3f(0.0, 0.0f ,4.0f );
-    glVertex3f(0.0, -1.0f ,3.0f );
     glEnd();
     glFlush();
 }
@@ -84,8 +76,47 @@ void reshapeFunc(int x, int y)
 
 void idleFunc(void)
 {
-    yRotated += 0.1;
+    xRotated += 0.0;
+    yRotated += 0.0;
+    zRotated += 0.0;
+    //redisplayFunc();
+}
+
+void look(int x, int y) {
+
+    if (is_first_time) {
+        time_old = high_resolution_clock::now();
+        prev_mouse_x = x;
+        prev_mouse_y = y;
+        is_first_time = false;
+        return;
+    }
+
+    high_resolution_clock::time_point time_new = high_resolution_clock::now();
+    duration<double, std::milli> dur = time_new - time_old;
+
+    cout << dur.count() << " milliseconds diff\n";
+
+    int delta_x = x - prev_mouse_x;
+    int delta_y = y - prev_mouse_y;
+    //int delta_z = x - prev_mouse_x;
+
+    //float angle_delta = atan2(abs(y - prev_mouse_y),abs(x - prev_mouse_x)) * 180/PI; 
+
+    prev_mouse_x = x;
+    prev_mouse_y = y;
+    time_old = time_new;
     redisplayFunc();
+}
+
+void keyboardFunc(unsigned char key, int x, int y) {
+
+    switch(key) {
+
+        case 'r': xRotated = yRotated = zRotated = 0; redisplayFunc(); break;
+        break;
+    };
+
 }
 
 int main (int argc, char **argv)
@@ -95,13 +126,13 @@ int main (int argc, char **argv)
     glutInitWindowSize(400,350);
     glutCreateWindow("Sphere Rotating Animation");
     glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-    xRotated = yRotated = zRotated = 30.0;
-    xRotated=33;
-    yRotated=40;
+    //xRotated = yRotated = zRotated = 0.0;
     glClearColor(0.0,0.0,0.0,0.0);
     glutDisplayFunc(redisplayFunc);
     glutReshapeFunc(reshapeFunc);
-    glutIdleFunc(idleFunc);
+    glutMotionFunc(look);
+    glutKeyboardFunc(keyboardFunc);
+    //glutIdleFunc(idleFunc);
     glutMainLoop();
     return 0;
 }
