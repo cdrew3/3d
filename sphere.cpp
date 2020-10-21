9642d0ad0bd31fcb65fc434ae477523b72f1627e
Chris Drew <chris.drew.cd@gmai.com>
2020-10-20 21:11:43 -0600

    More things and stuff

--- sphere.cpp
+++ sphere.cpp
@@ -7,7 +7,6 @@
 #include <cmath>
 #include <cstdio>
 #include <vector>
-#include <time.h>
 #include <chrono>
 
 #define PI 3.14159265
@@ -18,12 +17,12 @@ using namespace std::chrono;
 GLfloat xRotated, yRotated, zRotated;
 GLdouble radius=1;
 bool is_first_time = true;
+float rotx, roty, rotz;
 int prev_mouse_x, prev_mouse_y, dx, dy;
 vector<int> xarr;
 vector<int> yarr;
-high_resolution_clock::time_point time_old;
 double theta;
-int MAXARRSIZE = 10;
+int MAXARRSIZE = 5;
 
 double getAngle() {
     
@@ -101,17 +100,16 @@ void redisplayFunc(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     glTranslatef(0.0,0.0,-8.0);
-    glColor3f(0.2, 0.8, 0.1);
     gluLookAt(1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,1.0);
-    glRotatef(xRotated,1.0,0.0,0.0);
-    glRotatef(yRotated,0.0,1.0,0.0);
-    glRotatef(zRotated,0.0,0.0,1.0);
+    glColor3f(0.2, 0.8, 0.1);
+    glRotatef(xRotated,rotx,0.0,0.0);
+    glRotatef(yRotated,0.0,roty,0.0);
+    glRotatef(zRotated,0.0,0.0,rotz);
     glScalef(1.0,1.0,1.0);
     glutSolidSphere(radius,20,20);
     glFlush();
 
     drawAxes();
-
 }
 
 void reshapeFunc(int x, int y)
@@ -126,36 +124,36 @@ void reshapeFunc(int x, int y)
 
 void idleFunc(void)
 {
-    xRotated += 0.0;
-    yRotated += 0.0;
-    zRotated += 0.0;
-    //redisplayFunc();
+    //printf("---\nX: %f\nY: %f\nZ: %f\n", xRotated, yRotated, zRotated);
 }
 
 void look(int x, int y) {
 
     if (is_first_time) {
-        time_old = high_resolution_clock::now();
         prev_mouse_x = x;
         prev_mouse_y = y;
         is_first_time = false;
         return;
     }
 
-    high_resolution_clock::time_point time_new = high_resolution_clock::now();
-    duration<double, std::milli> dur = time_new - time_old;
-
-    //cout << dur.count() << " milliseconds diff\n";
+    int delta_x = x - prev_mouse_x;
+    int delta_y = y - prev_mouse_y;
     updateArr(x, y);
-    printArr("X", xarr);
-    printArr("Y", yarr);
-    double angle = getAngle();
-    if (abs(angle) > 30 || abs(angle) > 150) xRotated += 1;
-    if (abs(angle) > 60) yRotated += 1;
+
+    int dx = abs(xarr.back() - xarr.front());
+    int dy = abs(yarr.back() - yarr.front());
+    if (dx - dy > 3) {
+        xRotated += delta_x;
+        yRotated += delta_x;
+        zRotated += delta_x;
+    } else if (dy - dx > 3) {
+        xRotated += delta_y;
+        yRotated += delta_y;
+        zRotated += delta_y;
+    }
 
     prev_mouse_x = x;
     prev_mouse_y = y;
-    time_old = time_new;
     redisplayFunc();
 }
 
@@ -164,81 +162,42 @@ void keyboardFunc(unsigned char key, int x, int y) {
     switch(key) {
 
         case 'r': xRotated = yRotated = zRotated = 0; redisplayFunc(); break;
+        case 'd': redisplayFunc(); break;
+        case 'x': if (rotx == 0.0) rotx = 1.0; else rotx = 0.0; break;
+        case 'y': if (roty == 0.0) roty = 1.0; else roty = 0.0; break;
+        case 'z': if (rotz == 0.0) rotz = 1.0; else rotz = 0.0; break;
         break;
     };
 
 }
 
+void keyboardUpFunc(unsigned char key, int x, int y) {
+
+    switch(key) {
+
+        case 'x': rotx = 0.0; break;
+        case 'y': roty = 0.0; break;
+        case 'z': rotz = 0.0; break;
+        break;
+    };
+}
+
 int main (int argc, char **argv)
 {
+    rotx = roty = rotz = 0.0;
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(400,350);
     glutCreateWindow("Sphere Rotating Animation");
     glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-    //xRotated = yRotated = zRotated = 0.0;
     glClearColor(0.0,0.0,0.0,0.0);
-    glutDisplayFunc(redisplayFunc);
-    glutReshapeFunc(reshapeFunc);
     glutMotionFunc(look);
+    glutDisplayFunc(redisplayFunc);
+    glutIgnoreKeyRepeat(1);
+    glutReshapeFunc(reshapeFunc);
     glutKeyboardFunc(keyboardFunc);
-    glutIdleFunc(idleFunc);
+    //glutKeyboardUpFunc(keyboardUpFunc);
     glutMainLoop();
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include <iostream>
-#include <cstdlib>
-#include <GL/glut.h>
-using namespace std;
-
-static int win;
-
-void disp(void);
-void keyb(unsigned char key, int x, int y);
-
-int main(int argc, char **argv){
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
-glutInitWindowSize(500,500);
-win = glutCreateWindow("Here is a sphere, for some reason");
-glutDisplayFunc(disp);
-glutKeyboardFunc(keyb);
-glClearColor(0.0,0.0,0.0,0.0);
-glutMainLoop();
-return 0;
-}
-
-void disp(void){
-glClear(GL_COLOR_BUFFER_BIT);
-
-glutWireSphere(0.5,50,100);
-glutSwapBuffers();
-}
-
-void keyb(unsigned char key, int x, int y){
-if(key == 'q'){
-glutDestroyWindow(win);
-exit(0);
-}
-}
-*/
